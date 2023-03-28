965436da4fe178f6e98722ba0843e18e0ff15e00
nmazid121 <nmazid121@gmail.com>
2023-03-28 13:41:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("nabhan is retarded")
-print("george is retarded")
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])

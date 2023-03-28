632cc5a550fc0e49a526c73556b99d695258b971
nmazid121 <nmazid121@gmail.com>
2023-03-28 14:57:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import os
 os.environ['SPOTIPY_CLIENT_ID'] = 'f57033e4039647999c11f6bd7b026af8'
 os.environ['SPOTIPY_CLIENT_SECRET'] = '339191ccaa2f4f7eb450a8ff4aabc04c'
 
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+artist_name = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-results = spotify.artist_top_tracks(lz_uri)
+results = spotify.artist_top_tracks(artist_name)
 
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print()
+playlist_id = 'spotify:playlist:013wGmqopEBhV6hPKGAgV0?si=1ada76a979aa486c'
+username = 'drypdrop'
+
+playlist = spotify.user_playlist(username, playlist_id)
+
+playlist_name = playlist['name']
+print(f"Playlist name: {playlist_name}")
